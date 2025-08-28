42b1e9771cb19bff42f05ee8eb8eaa9ee3050337
mjh-shikder <mjhsikder@gmail.com>
2025-08-28 14:36:14 +0600

    Card heart button click action fixed

--- scrip.js
+++ scrip.js
@@ -3,50 +3,68 @@ let heartCount = document.getElementById('heart-count');
 
 let count = 0; 
 
-document.getElementById('card-hart-icon1').addEventListener('click', function () {
-    count = count + 1; 
-    heartCount.innerText = count;
-})
+function cartHeartButtons(id) {
+    document.getElementById(id).addEventListener('click', function () {
+        count = count + 1; 
+        heartCount.innerText = count; 
+        
+    })
+}
 
-document.getElementById('card-hart-icon2').addEventListener('click', function () {
-    count = count + 1; 
-    heartCount.innerText = count;
-})
+cartHeartButtons('card-hart-icon1')
+cartHeartButtons('card-hart-icon2')
+cartHeartButtons('card-hart-icon3')
+cartHeartButtons('card-hart-icon4')
+cartHeartButtons('card-hart-icon5')
+cartHeartButtons('card-hart-icon6')
+cartHeartButtons('card-hart-icon7')
+cartHeartButtons('card-hart-icon8')
+cartHeartButtons('card-hart-icon9')
 
-document.getElementById('card-hart-icon3').addEventListener('click', function () {
-    count = count + 1; 
-    heartCount.innerText = count;
-})
+// document.getElementById('card-hart-icon1').addEventListener('click', function () {
+//     count = count + 1; 
+//     heartCount.innerText = count;
+// })
 
-document.getElementById('card-hart-icon4').addEventListener('click', function () {
-    count = count + 1; 
-    heartCount.innerText = count;
-})
+// document.getElementById('card-hart-icon2').addEventListener('click', function () {
+//     count = count + 1; 
+//     heartCount.innerText = count;
+// })
 
-document.getElementById('card-hart-icon5').addEventListener('click', function () {
-    count = count + 1; 
-    heartCount.innerText = count;
-})
+// document.getElementById('card-hart-icon3').addEventListener('click', function () {
+//     count = count + 1; 
+//     heartCount.innerText = count;
+// })
 
-document.getElementById('card-hart-icon6').addEventListener('click', function () {
-    count = count + 1; 
-    heartCount.innerText = count;
-})
+// document.getElementById('card-hart-icon4').addEventListener('click', function () {
+//     count = count + 1; 
+//     heartCount.innerText = count;
+// })
 
-document.getElementById('card-hart-icon7').addEventListener('click', function () {
-    count = count + 1; 
-    heartCount.innerText = count;
-})
+// document.getElementById('card-hart-icon5').addEventListener('click', function () {
+//     count = count + 1; 
+//     heartCount.innerText = count;
+// })
 
-document.getElementById('card-hart-icon8').addEventListener('click', function () {
-    count = count + 1; 
-    heartCount.innerText = count;
-})
+// document.getElementById('card-hart-icon6').addEventListener('click', function () {
+//     count = count + 1; 
+//     heartCount.innerText = count;
+// })
 
-document.getElementById('card-hart-icon9').addEventListener('click', function () {
-    count = count + 1; 
-    heartCount.innerText = count;
-})
+// document.getElementById('card-hart-icon7').addEventListener('click', function () {
+//     count = count + 1; 
+//     heartCount.innerText = count;
+// })
+
+// document.getElementById('card-hart-icon8').addEventListener('click', function () {
+//     count = count + 1; 
+//     heartCount.innerText = count;
+// })
+
+// document.getElementById('card-hart-icon9').addEventListener('click', function () {
+//     count = count + 1; 
+//     heartCount.innerText = count;
+// })
 
 
 
