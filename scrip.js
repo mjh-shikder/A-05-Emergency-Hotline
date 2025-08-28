d8e038e4ca5c90826f7d2dc3a13efaa8f5fa538a
mjh-shikder <mjhsikder@gmail.com>
2025-08-28 14:04:14 +0600

    card heart icon click funtion added

--- scrip.js
+++ scrip.js
@@ -1,1 +1,22 @@
-console.log('js is conneted baby');
+// Hear count 
+let heartCountElement = document.getElementById('heart-count');
+
+let count = 0;
+// global function for card hart button click
+function cardHeartButton(id) {
+    document.getElementById(id).addEventListener('click', function () {
+        count = count + 1;
+        heartCountElement.innerText = count;
+    })
+}
+
+cardHeartButton('card-hart-icon1');
+cardHeartButton('card-hart-icon2');
+cardHeartButton('card-hart-icon3');
+cardHeartButton('card-hart-icon4');
+cardHeartButton('card-hart-icon5');
+cardHeartButton('card-hart-icon6');
+cardHeartButton('card-hart-icon7');
+cardHeartButton('card-hart-icon8');
+cardHeartButton('card-hart-icon9');
+
