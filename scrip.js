b1cf4a41c8b8294a5fd1e8f7fb2c6ea97e03f8d9
mjh-shikder <mjhsikder@gmail.com>
2025-08-28 14:29:23 +0600

    Card heart button click action fixed

--- scrip.js
+++ scrip.js
@@ -1,25 +1,59 @@
 // Heart count 
-let heartCountElement = document.getElementById('heart-count');
+let heartCount = document.getElementById('heart-count');
 
-let count = document.getElementById('heart-count').innerText;
-count = 0;
+let count = 0; 
+
+document.getElementById('card-hart-icon1').addEventListener('click', function () {
+    count = count + 1; 
+    heartCount.innerText = count;
+})
+
+document.getElementById('card-hart-icon2').addEventListener('click', function () {
+    count = count + 1; 
+    heartCount.innerText = count;
+})
+
+document.getElementById('card-hart-icon3').addEventListener('click', function () {
+    count = count + 1; 
+    heartCount.innerText = count;
+})
+
+document.getElementById('card-hart-icon4').addEventListener('click', function () {
+    count = count + 1; 
+    heartCount.innerText = count;
+})
+
+document.getElementById('card-hart-icon5').addEventListener('click', function () {
+    count = count + 1; 
+    heartCount.innerText = count;
+})
+
+document.getElementById('card-hart-icon6').addEventListener('click', function () {
+    count = count + 1; 
+    heartCount.innerText = count;
+})
+
+document.getElementById('card-hart-icon7').addEventListener('click', function () {
+    count = count + 1; 
+    heartCount.innerText = count;
+})
+
+document.getElementById('card-hart-icon8').addEventListener('click', function () {
+    count = count + 1; 
+    heartCount.innerText = count;
+})
+
+document.getElementById('card-hart-icon9').addEventListener('click', function () {
+    count = count + 1; 
+    heartCount.innerText = count;
+})
 
 
-// global function for card hart button click
-function cardHeartButton(id) {
-    document.getElementById(id).addEventListener('click', function () {
-        count = count + 1;
-        heartCountElement.innerText = count;
-    })
-}
 
-cardHeartButton('card-hart-icon1');
-cardHeartButton('card-hart-icon2');
-cardHeartButton('card-hart-icon3');
-cardHeartButton('card-hart-icon4');
-cardHeartButton('card-hart-icon5');
-cardHeartButton('card-hart-icon6');
-cardHeartButton('card-hart-icon7');
-cardHeartButton('card-hart-icon8');
-cardHeartButton('card-hart-icon9');
+
+
+
+
+
+
 
