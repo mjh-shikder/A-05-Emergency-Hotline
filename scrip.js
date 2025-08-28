8877a5b958e1e689cdb4e24fd9904258bb0a14c6
mjh-shikder <mjhsikder@gmail.com>
2025-08-28 14:39:04 +0600

    Card heart button click with reusable function

--- scrip.js
+++ scrip.js
@@ -21,50 +21,7 @@ cartHeartButtons('card-hart-icon7')
 cartHeartButtons('card-hart-icon8')
 cartHeartButtons('card-hart-icon9')
 
-// document.getElementById('card-hart-icon1').addEventListener('click', function () {
-//     count = count + 1; 
-//     heartCount.innerText = count;
-// })
 
-// document.getElementById('card-hart-icon2').addEventListener('click', function () {
-//     count = count + 1; 
-//     heartCount.innerText = count;
-// })
-
-// document.getElementById('card-hart-icon3').addEventListener('click', function () {
-//     count = count + 1; 
-//     heartCount.innerText = count;
-// })
-
-// document.getElementById('card-hart-icon4').addEventListener('click', function () {
-//     count = count + 1; 
-//     heartCount.innerText = count;
-// })
-
-// document.getElementById('card-hart-icon5').addEventListener('click', function () {
-//     count = count + 1; 
-//     heartCount.innerText = count;
-// })
-
-// document.getElementById('card-hart-icon6').addEventListener('click', function () {
-//     count = count + 1; 
-//     heartCount.innerText = count;
-// })
-
-// document.getElementById('card-hart-icon7').addEventListener('click', function () {
-//     count = count + 1; 
-//     heartCount.innerText = count;
-// })
-
-// document.getElementById('card-hart-icon8').addEventListener('click', function () {
-//     count = count + 1; 
-//     heartCount.innerText = count;
-// })
-
-// document.getElementById('card-hart-icon9').addEventListener('click', function () {
-//     count = count + 1; 
-//     heartCount.innerText = count;
-// })
 
 
 
